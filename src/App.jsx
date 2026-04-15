d63c4e1d348590ed11b9c8c62f1eb3540981d185
Filip Oltei <filipolt@yahoo.ro>
2026-04-15 13:11:50 +0300

    V8

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import Card from './Card';
+
+
 function App() {
  return (
  <div>
  <h1>Dashboard</h1>
  <p>Oltei Filip </p>
+
+ <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
+<Card title="Proiect 2" description="Pagina interactiva cu JavaScript" />
+<Card title="Proiect 3" description="Dashboard cu React" />
+ 
+
+ <Card />
  </div>
  );
-}
 
+}
+export default App
 
